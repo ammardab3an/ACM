885f215d6b713d3db5cee1e4a49f7cb0d6341ffe
ammardab3an <ammar.dabaan99@gmail.com>
2020-11-12 12:50:29 +0200

    backup

--- notes/main_cp.cpp
+++ notes/main_cp.cpp
@@ -17,17 +17,23 @@ typedef vector<lli>         vl;
 typedef vector<pii>         vpii;
 typedef vector<pll>         vpll;
 
+//#define int int64_t
+
 #define endl '\n'
 #define fastIO ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
 
-const int MAX = 2e5 + 10;
 const int INF = 0x3f3f3f3f;
+const int MOD = 1e9 + 7;
+const int NMAX = 2e5 + 10;
+const int MMAX = 2e5 + 10;
 
 
-int main(){
+
+int32_t main(){
     
     fastIO;
-    freopen("input.in", "r", stdin);
+    //freopen("input.in", "r", stdin);
+	//freopen("output.o", "w", stdout);
 	
 	int t; cin >> t; while(t--){
 		
