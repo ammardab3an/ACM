ff84455fe92d5489a5f4b44dd6b5fdfc656f4acc
ammardab3an <ammar.dabaan99@gmail.com>
2021-07-06 19:37:21 +0300

    backup

---
+++ problems/done/codeforces/D. Deleting Divisors.cpp
@@ -0,0 +1,128 @@
+// Problem: D. Deleting Divisors
+// Contest: Codeforces - Codeforces Round #726 (Div. 2)
+// URL: https://codeforces.com/contest/1537/problem/D
+// Memory Limit: 256 MB
+// Time Limit: 2000 ms
+// 
+// Powered by CP Editor (https://cpeditor.org)
+
+
+// By AmmarDab3an - Aleppo University
+
+#include "bits/stdc++.h"
+
+using namespace std;
+
+#define int int64_t
+#define ll  int64_t
+
+// typedef unsigned int        uint;
+// typedef long long int       ll;
+// typedef unsigned long long  ull;
+typedef pair<int, int>    pii;
+typedef pair<ll, ll>      pll;
+typedef pair<int, pii>    iii;
+typedef pair<ll, pll>     lll;
+typedef vector<int>       vi;
+typedef vector<ll>        vl;
+typedef vector<pii>       vpii;
+typedef vector<pll>       vpll;
+
+#define endl '\n'
+#define fastIO ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+#define freopenI freopen("input.txt", "r", stdin);
+#define freopenO freopen("output.txt", "w", stdout);
+
+const int INF = 0x3f3f3f3f;
+const ll INFLL = 0x3f3f3f3f3f3f3f3f;
+const int MOD = 1e9 + 7;
+const double EPS = 1e-9;
+const double  PI = acos(-1);
+
+mt19937 rng(chrono::steady_clock::now().time_since_epoch().count());
+ 
+int rand(int x, int y) {
+    return uniform_int_distribution<int>(x, y)(rng);
+}
+
+int mul(int a, int b){
+	return (1ll * a * b) % MOD;
+}
+ 
+int add(int a, int b){
+	return (1ll * a + b + MOD + MOD) % MOD;
+}
+ 
+int pow_exp(int n, int p){
+	if(!p) return 1;
+	if(p&1) return mul(n, pow_exp(n, p-1));
+	int tmp = pow_exp(n, p/2);
+	return mul(tmp, tmp);
+}
+ 
+const int  MAX = 2e5 + 10;
+const int NMAX = 2e5 + 10;
+const int MMAX = 2e5 + 10;
+const int LOG_MAX = ceil(log2(double(NMAX)));
+const int BLOCK = ceil(sqrt(double(NMAX)));
+
+int mem[NMAX];
+int go(int cur){
+	
+	int &ret = mem[cur];
+	if(ret+1) return ret;
+	
+	int ans = false;
+	
+	for(int i = 2; i*i <= cur; i++){
+		
+		if(cur%i==0){
+			if(!go(cur-i)){
+				ans = true;
+				break;
+			}
+			if(!go(cur-cur/i)){
+				ans = true;
+				break;
+			}
+		}
+	}	
+	
+	return ret = ans;
+}
+
+int32_t main(){
+    
+    fastIO;
+    
+#ifdef LOCAL
+    freopenI;
+    freopenO;
+#endif
+
+    // freopen("name.in", "r", stdin);
+    
+    int t; cin >> t; while(t--){
+
+		int n;
+		cin >> n;
+		
+		int _n = n;
+		
+		int cnt_2 = 0;
+		while(_n%2 == 0){
+			cnt_2++;
+			_n /= 2;
+		}
+		
+		if(!cnt_2){
+			cout << "Bob" << endl;
+		}
+		else if(_n == 1){
+			cout << (cnt_2%2==0 ? "Alice" : "Bob") << endl;
+		}
+		else{
+			cout << "Alice" << endl;
+		}
+    }	
+}
