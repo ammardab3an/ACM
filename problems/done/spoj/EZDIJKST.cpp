2d9e6806aac180de4954bc3182afbdf7cbd8873b
AmmarDab3an <ammar.dabaan99@gmail.com>
2022-02-05 14:14:26 +0200

    fix spoj folder

--- problems/done/spoj/EZDIJKST.cpp
+++ problems/done/spoj/EZDIJKST.cpp
@@ -1,118 +1,62 @@
-// Problem: Easy Dijkstra Problem
-// Contest: SPOJ - Tutorial
-// URL: https://www.spoj.com/problems/EZDIJKST/
-// Memory Limit: 1536 MB
-// Time Limit: 1000 ms
-// 
-// Powered by CP Editor (https://cpeditor.org)
-
-
-// By AmmarDab3an - Aleppo University
-
-#include "bits/stdc++.h"
+#include <bits/stdc++.h>
 
 using namespace std;
 
-//#define int int64_t
-//#define lli int64_t
+#define MAX 10010
+#define pii pair<int, int>
+#define weight  first
+#define id      second
 
-typedef unsigned int        uint;
-typedef long long int       lli;
-typedef unsigned long long  ull;
-typedef pair<int, int>      pii;
-typedef pair<lli, lli>      pll;
-typedef pair<int, pii>      iii;
-typedef pair<lli, pll>      lll;
-typedef vector<int>         vi;
-typedef vector<lli>         vl;
-typedef vector<pii>         vpii;
-typedef vector<pll>         vpll;
+int A, B;
+array<vector<pii>, MAX> adj;
 
-#define endl '\n'
-#define fastIO ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
-#define freopenI freopen("input.txt", "r", stdin);
-#define freopenO freopen("output.txt", "w", stdout);
+void dij()
+{
+    array<int, MAX> dist;
+    dist.fill(INT_MAX);
+    dist[A] = 0;
 
-const int INF = 0x3f3f3f3f;
-const lli INFLL = 0x3f3f3f3f3f3f3f3f;
-const int MOD = 1e9 + 7;
-const double EPS = 1e-9;
-const double  PI = acos(-1);
+    priority_queue<pii, vector<pii>, greater<pii> > que; que.push({0, A});
 
-const int  MAX = 2e5 + 10;
-const int NMAX = 2e5 + 10;
-const int MMAX = 2e5 + 10;
+    while(!que.empty())
+    {
+        pii cur = que.top(); que.pop();
 
-int n, m;
-vector<pii> adj[10100];
-int dis[10100];
+        if(dist[cur.id] < cur.weight)
+            continue;
 
-int dij(int u, int v){
-    
-     for(int i = 1; i <= n; i++) dis[i] = INF;
-     
-     dis[u] = 0;
-     priority_queue<pii> que;
-     que.push({0, u});
-     
-     while(!que.empty()){
-         
-         pii tp = que.top(); que.pop();
-         
-         int cnd = tp.second;
-         int cw = -1 * tp.first;
-         
-         if(dis[cnd] < cw) continue;
-         
-         if(cnd == v) return cw;
-         
-         for(auto nxt : adj[cnd]){
-             
-             int nxt_nd = nxt.second;
-             int nxt_dis = cw + nxt.first;
-             
-             if(dis[nxt_nd] > nxt_dis){
-                 
-                 dis[nxt_nd] = nxt_dis;
-                 que.push({-1 * nxt_dis, nxt_nd});
-             }
-         }
-     }
-     
-     return -1;
+        if(cur.id == B)
+        {
+            cout << cur.weight << endl;
+            return;
+        }
+
+        for(auto to : adj[cur.id])
+        {
+            if(dist[to.id] > cur.weight + to.weight)
+            {
+                dist[to.id] = cur.weight + to.weight;
+                que.push({dist[to.id], to.id});
+            }
+        }
+    }
+    cout << "NO" << endl;
 }
+int main()
+{
+    int T, V, K, a, b, c;
 
-int32_t main(){
-    
-    fastIO;
-    
-#ifdef LOCAL
-    freopenI;
-    freopenO;
-#endif
+    for(cin >> T; T; T--)
+    {
+        adj.fill({});
 
-    int t; cin >> t; while(t--){
-        
-        cin >> n >> m;
-        
-        for(int i = 1; i <= n; i++) adj[i].clear();
-        
-        for(int i = 0; i < m; i++){
-            int u, v, w;
-            cin >> u >> v >> w;
-            adj[u].push_back({w, v});
+        for(cin >> V >> K; K; K--)
+        {
+            cin >> a >> b >> c;
+            adj[a].push_back({c, b});
         }
-        
-        int u, v;
-        cin >> u >> v;
-        
-        int dis = dij(u, v);
-        
-        if(dis == -1){
-            cout << "NO" << endl;
-        }
-        else{
-            cout << dis << endl;
-        }
-    }	
+
+        cin >> A >> B;
+        dij();
+    }
 }
